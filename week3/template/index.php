7fef467bf8938fc4d103d983bc19403acaba2cce
Yazid Nabil Mubarok <114566695+yaz2302@users.noreply.github.com>
2023-04-09 02:09:20 +0700

    Add files via upload

---
+++ week3/template/index.php
@@ -0,0 +1,12 @@
+<!-- panggil file atas.php -->
+<?php
+require_once('atas.php');
+?>
+<!-- panggil file content.php -->
+<?php
+require_once('content.php');
+?>
+<!-- panggil file bawah.php -->
+<?php
+require_once('bawah.php');
+?>
