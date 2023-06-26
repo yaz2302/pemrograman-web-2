63d4372f15f9891774cb4635c6292e3683a841a3
Yazid Nabil Mubarok <114566695+yaz2302@users.noreply.github.com>
2023-06-26 19:14:52 +0700

    Delete week3 directory

--- week3/template/content.php
+++
@@ -1,8 +0,0 @@
-<div id="layoutSidenav_content">
-    <main>
-        <div class="container-fluid px-4">
-            <h1 class="mt-4">Dashboard</h1>
-            <p>Hello World ini adalah dashboard</p>
-        </div>
-    </main>
-</div>
